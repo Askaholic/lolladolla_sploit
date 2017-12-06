af62789d6c6327d51751038d1d62160b918875dc
Dope Beats <dope@dope-beats.com>
2017-12-05 19:04:25 -0900

    Added blank module that just prints

--- lolladolla_scanner.rb
+++ lolladolla_scanner.rb
@@ -1,1 +1,25 @@
-/root/.msf4/modules/auxiliary/scanner/lolladolla/lolladolla_scanner.rb
+require 'msf/core'
+
+class MetasploitModule < Msf::Auxiliary
+	
+	include Msf::Auxiliary::Scanner
+
+	def initialize
+
+		super(
+			'Name' => 'LollaDolla Scanna',
+			'Version' => 'v1',
+			'Description' => 'Scan the lolladolla server',
+			'Author' => 'Dope Beats',
+			'License' => MSF_LICENSE
+		)
+	
+		deregister_options('RPORT', 'RHOST')
+	end
+
+	def run_host(ip)
+		begin
+			puts "Scanned..."
+		end
+	end
+end
